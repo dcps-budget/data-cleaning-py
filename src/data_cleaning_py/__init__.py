3e40ecd778690c8cb0c2127621e189879db999ed
stevenprimeaux-k12dc <steven.primeaux@k12.dc.gov>
2025-07-18 16:12:40 -0400

    Add step to update existing Quickbase records.

--- src/data_cleaning_py/__init__.py
+++ src/data_cleaning_py/__init__.py
@@ -1,32 +1,27 @@
 from datetime import datetime
 from data_cleaning_py import clean_025, quickbase
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 def main() -> None:
     print("Job is running!")
 
-    fields_join = [
-        "year_fiscal",
-        "agency_id",
-        "fund_id",
-        "program_id",
-        "costcenter_id",
-        "account_id",
-        "project_id",
-        "award_id",
-    ]
-
-    fields = quickbase.qb_fields()
-
-    balances_difs = clean_025.clean_025()
     balances_qb = quickbase.qb_balances()
-    balances = balances_qb.merge(balances_difs, how="outer", on=fields_join)
-    missing_from_qb = (
-        balances.loc[lambda df: df["qbid"].isna()]
-        .drop(columns=["qbid"])
-        .rename(columns=fields)
-        .to_dict(orient="records")
-    )
-    quickbase.qb_balances_insert(missing_from_qb)
+    balances_difs = clean_025.clean_025()
 
-    print("Finished", "overall job", "at", datetime.now())
+    balances = quickbase.qb_balances_merge(
+        balances_qb=balances_qb, balances_difs=balances_difs
+    )
+
+    missing_qb = quickbase.balances_missing_qb(balances=balances)
+    if len(missing_qb) > 0:
+        quickbase.qb_balances_insert(missing_qb)
+
+    present_qb = quickbase.balances_present_qb(balances=balances)
+    if len(present_qb) > 0:
+        quickbase.qb_balances_update(present_qb)
+
+    print("Finished overall job at", datetime.now())
--- src/data_cleaning_py/quickbase.py
+++ src/data_cleaning_py/quickbase.py
@@ -1,10 +1,6 @@
 import os
 import pandas as pd
 import requests
-from dotenv import load_dotenv
-
-
-load_dotenv()
 
 
 def qb_headers():
@@ -14,6 +10,19 @@ def qb_headers():
         "Authorization": f"QB-USER-TOKEN {os.getenv('QB_AUTHORIZATION')}",
         "Content-Type": "application/json",
     }
+
+
+def qb_balances_fields_join():
+    return [
+        "year_fiscal",
+        "agency_id",
+        "fund_id",
+        "program_id",
+        "costcenter_id",
+        "account_id",
+        "project_id",
+        "award_id",
+    ]
 
 
 def qb_fields():
@@ -121,4 +130,54 @@ def qb_balances_insert(balances):
         url="https://api.quickbase.com/v1/records", json=body, headers=qb_headers()
     )
 
-    print(r.json())
+    print("Inserted", len(r.json()["metadata"]["createdRecordIds"]), "records")
+
+
+def qb_balances_merge(balances_qb, balances_difs):
+    balances = balances_qb.merge(
+        balances_difs, how="outer", on=qb_balances_fields_join()
+    )
+
+    print("Found", len(balances), "records total between Quickbase and DIFS")
+
+    return balances
+
+
+def balances_missing_qb(balances):
+    missing_qb = (
+        balances.loc[lambda df: df["qbid"].isna()]
+        .drop(columns=["qbid"])
+        .rename(columns=qb_fields())
+        .to_dict(orient="records")
+    )
+
+    print("Found", len(missing_qb), "records missing in Quickbase")
+
+    return missing_qb
+
+
+def balances_present_qb(balances):
+    present_qb = (
+        balances.loc[lambda df: df["qbid"].notna()]
+        .rename(columns=qb_fields())
+        .to_dict(orient="records")
+    )
+
+    print("Found", len(present_qb), "records present in Quickbase")
+
+    return present_qb
+
+
+def qb_balances_update(balances):
+    data = []
+    for row in balances:
+        data.append({k: {"value": v} for k, v in row.items()})
+
+    body = {"to": "bu9duyip8", "data": data, "mergeFieldId": 3}
+
+    r = requests.post(
+        url="https://api.quickbase.com/v1/records", json=body, headers=qb_headers()
+    )
+
+    print("Updated", len(r.json()["metadata"]["updatedRecordIds"]), "records")
+    print("Left", len(r.json()["metadata"]["unchangedRecordIds"]), "records unchanged")
