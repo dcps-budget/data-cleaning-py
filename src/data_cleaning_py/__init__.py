0ca2cc0f44f7277a8f6fca3ae57de40304145da6
stevenprimeaux-k12dc <steven.primeaux@k12.dc.gov>
2025-07-22 21:31:49 -0400

    Refactor and clean up.

--- src/data_cleaning_py/__init__.py
+++ src/data_cleaning_py/__init__.py
@@ -9,19 +9,19 @@ load_dotenv()
 def main() -> None:
     print("Job is running!")
 
-    balances_qb = quickbase.qb_balances()
+    balances_qb = quickbase.qb_get_balances()
     balances_difs = clean_025.clean_025()
 
-    balances = quickbase.qb_balances_merge(
+    balances = quickbase.qb_merge_balances(
         balances_qb=balances_qb, balances_difs=balances_difs
     )
 
     missing_qb = quickbase.balances_missing_qb(balances=balances)
     if len(missing_qb) > 0:
-        quickbase.qb_balances_insert(missing_qb)
+        quickbase.qb_insert_balances(missing_qb)
 
     present_qb = quickbase.balances_present_qb(balances=balances)
     if len(present_qb) > 0:
-        quickbase.qb_balances_update(present_qb)
+        quickbase.qb_update_balances(present_qb)
 
     print("Finished overall job at", datetime.now())
--- src/data_cleaning_py/quickbase.py
+++ src/data_cleaning_py/quickbase.py
@@ -3,16 +3,16 @@ import pandas as pd
 import requests
 
 
-def qb_headers():
+def qb_headers() -> dict:
     return {
-        "QB-Realm-Hostname": os.getenv("QB_REALMHOSTNAME"),
+        "QB-Realm-Hostname": os.getenv("QB_QBREALMHOSTNAME"),
         "User-Agent": os.getenv("QB_USERAGENT"),
         "Authorization": f"QB-USER-TOKEN {os.getenv('QB_AUTHORIZATION')}",
         "Content-Type": "application/json",
     }
 
 
-def qb_balances_fields_join():
+def qb_balances_fields_join() -> list[str]:
     return [
         "year_fiscal",
         "agency_id",
@@ -25,28 +25,28 @@ def qb_balances_fields_join():
     ]
 
 
-def qb_fields():
-    fields_join = [
-        "year_fiscal",
-        "agency_id",
-        "fund_id",
-        "program_id",
-        "costcenter_id",
-        "account_id",
-        "project_id",
-        "award_id",
+def qb_balances_fields_query() -> list[str]:
+    return qb_balances_fields_join() + [
+        "qbid",
     ]
 
-    fields_insert = fields_join + [
+
+def qb_balances_fields_insert() -> list[str]:
+    return qb_balances_fields_join() + [
         "account_3_id",
         "account_1_id",
         "budget_adjusted",
         "budget_spent",
         "budget_current",
     ]
-    fields_all = fields_insert + ["qbid"]
 
-    params = {"tableId": "bu9duyip8"}
+
+def qb_balances_fields_all() -> list[str]:
+    return qb_balances_fields_insert() + ["qbid"]
+
+
+def qb_getfields_balances() -> dict:
+    params = {"tableId": os.getenv("QB_TABLEID_BALANCES")}
 
     r = requests.get(
         url="https://api.quickbase.com/v1/fields", params=params, headers=qb_headers()
@@ -55,85 +55,40 @@ def qb_fields():
     return {
         field["label"]: field["id"]
         for field in r.json()
-        if field["label"] in fields_all
+        if field["label"] in qb_balances_fields_all()
     }
 
 
-def qb_balances():
-    fields_join = [
-        "year_fiscal",
-        "agency_id",
-        "fund_id",
-        "program_id",
-        "costcenter_id",
-        "account_id",
-        "project_id",
-        "award_id",
-    ]
-
-    fields_query = fields_join + ["qbid"]
-
-    fields_insert = fields_join + [
-        "account_3_id",
-        "account_1_id",
-        "budget_adjusted",
-        "budget_spent",
-        "budget_current",
-    ]
-    fields_all = fields_insert + ["qbid"]
-
-    params = {"tableId": "bu9duyip8"}
-
-    r = requests.get(
-        url="https://api.quickbase.com/v1/fields", params=params, headers=qb_headers()
+def qb_get_balances() -> pd.DataFrame:
+    fields = qb_getfields_balances()
+    field_ids = {v: k for k, v in fields.items()}
+    field_ids_query = list(
+        {v: k for k, v in fields.items() if k in qb_balances_fields_query()}
     )
 
-    fields = {
-        field["label"]: field["id"]
-        for field in r.json()
-        if field["label"] in fields_all
+    body = {
+        "from": os.getenv("QB_TABLEID_BALANCES"),
+        "select": field_ids_query,
+        "where": f"{{{fields['year_fiscal']}.EX.{os.getenv('YEAR_FISCAL')}}}",
     }
-
-    field_ids = {v: k for k, v in fields.items()}
-
-    field_ids_query = list({v: k for k, v in fields.items() if k in fields_query})
-
-    body = {"from": "bu9duyip8", "select": field_ids_query}
 
     r = requests.post(
         url="https://api.quickbase.com/v1/records/query",
         json=body,
         headers=qb_headers(),
     )
+    data = r.json()["data"]
 
-    balances = r.json()["data"]
-
-    balances_normalized = {}
+    table = {}
     for field_id in field_ids_query:
-        balances_normalized[field_id] = []
-        for balance in balances:
-            balances_normalized[field_id].append(balance[str(field_id)]["value"])
+        table[field_id] = [row[str(field_id)]["value"] for row in data]
 
-    balances_normalized = pd.DataFrame(balances_normalized).rename(columns=field_ids)
-
-    return balances_normalized
+    return pd.DataFrame(table).rename(columns=field_ids)
 
 
-def qb_balances_insert(balances):
-    data = []
-    for row in balances:
-        data.append({k: {"value": v} for k, v in row.items()})
-
-    body = {"to": "bu9duyip8", "data": data}
-
-    r = requests.post(
-        url="https://api.quickbase.com/v1/records", json=body, headers=qb_headers()
-    )
-
-    print("Inserted", len(r.json()["metadata"]["createdRecordIds"]), "records")
-
-
-def qb_balances_merge(balances_qb, balances_difs):
+def qb_merge_balances(
+    balances_qb: pd.DataFrame, balances_difs: pd.DataFrame
+) -> pd.DataFrame:
     balances = balances_qb.merge(
         balances_difs, how="outer", on=qb_balances_fields_join()
     )
@@ -143,12 +98,11 @@ def qb_balances_merge(balances_qb, balances_difs):
     return balances
 
 
-def balances_missing_qb(balances):
+def balances_missing_qb(balances: pd.DataFrame) -> pd.DataFrame:
     missing_qb = (
         balances.loc[lambda df: df["qbid"].isna()]
         .drop(columns=["qbid"])
-        .rename(columns=qb_fields())
-        .to_dict(orient="records")
+        .rename(columns=qb_getfields_balances())
     )
 
     print("Found", len(missing_qb), "records missing in Quickbase")
@@ -156,11 +110,9 @@ def balances_missing_qb(balances):
     return missing_qb
 
 
-def balances_present_qb(balances):
-    present_qb = (
-        balances.loc[lambda df: df["qbid"].notna()]
-        .rename(columns=qb_fields())
-        .to_dict(orient="records")
+def balances_present_qb(balances: pd.DataFrame) -> pd.DataFrame:
+    present_qb = balances.loc[lambda df: df["qbid"].notna()].rename(
+        columns=qb_getfields_balances()
     )
 
     print("Found", len(present_qb), "records present in Quickbase")
@@ -168,12 +120,26 @@ def balances_present_qb(balances):
     return present_qb
 
 
-def qb_balances_update(balances):
-    data = []
-    for row in balances:
-        data.append({k: {"value": v} for k, v in row.items()})
+def qb_insert_balances(table: pd.DataFrame) -> None:
+    data = [
+        {k: {"value": v} for k, v in row.items()}
+        for row in table.to_dict(orient="records")
+    ]
+    body = {"to": os.getenv("QB_TABLEID_BALANCES"), "data": data}
 
-    body = {"to": "bu9duyip8", "data": data, "mergeFieldId": 3}
+    r = requests.post(
+        url="https://api.quickbase.com/v1/records", json=body, headers=qb_headers()
+    )
+
+    print("Inserted", len(r.json()["metadata"]["createdRecordIds"]), "records")
+
+
+def qb_update_balances(table: pd.DataFrame) -> None:
+    data = [
+        {k: {"value": v} for k, v in row.items()}
+        for row in table.to_dict(orient="records")
+    ]
+    body = {"to": os.getenv("QB_TABLEID_BALANCES"), "data": data, "mergeFieldId": 3}
 
     r = requests.post(
         url="https://api.quickbase.com/v1/records", json=body, headers=qb_headers()
